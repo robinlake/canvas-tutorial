c2d2f34a3cea7911170a5b58e5d6068609abfccb
Robin Lake <robin@tempus-ex.com>
2022-05-18 16:03:03 -0700

    wip

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d');
 
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
 c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.5)';
 c.fillRect(300, 300, 100, 100);
 
 // https://www.youtube.com/watch?v=83L6B13ixQ0&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL&index=2
@@ -17,3 +20,18 @@ c.lineTo(300, 100);
 c.lineTo(400, 300);
 c.strokeStyle = "#fa34a3";
 c.stroke();
+
+// Arc/Circle
+c.beginPath();
+c.arc(300, 300, 30, 0, Math.PI * 2, false);
+c.strokeStyle = 'blue';
+c.stroke();
+
+for (let i = 0; i < 30; i++) {
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    c.beginPath();
+    c.arc(x, y, 30, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+}
