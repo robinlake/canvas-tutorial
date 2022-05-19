354b936cd1ac07188836891f20ce2f7cab5f4e6b
Robin Lake <robin@tempus-ex.com>
2022-05-18 17:00:37 -0700

    end of video 3

--- index.js
+++ index.js
@@ -32,28 +32,58 @@ const c = canvas.getContext('2d');
 // }
 
 // https://www.youtube.com/watch?v=yq2au9EfeRQ&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL&index=3
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) * 8;
-let dy = (Math.random() - 0.5) * 8;
-let radius = 30;
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+    }
+
+    this.update = function() {
+
+        if (this.x + radius > innerWidth || this.x - radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + radius > innerHeight || this.y - radius < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+
+
+const circleArray = [];
+
+for (let i = 0; i < 100; i++) {
+    let radius = 30;
+    let x = Math.random() * (innerWidth - radius * 2) + radius;
+    let y = Math.random() * (innerHeight - radius * 2) + radius;
+    let dx = (Math.random() - 0.5) * 8;
+    let dy = (Math.random() - 0.5) * 8;
+    circleArray.push(new Circle(x, y, dx, dy, radius))
+}
+
+
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    // Arc/Circle
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
 
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
+    for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
     }
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
-    x += dx;
-    y += dy;
 }
 
 animate();
